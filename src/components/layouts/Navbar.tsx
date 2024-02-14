ba6af898e8aeb3bf8809121c251e6806aed801f5
ThanaponChanlaor <108111367+ThanaponChanlaor@users.noreply.github.com>
2024-02-14 11:35:22 +0700

    custom logo

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -2,8 +2,8 @@ import logo from '../../logo.svg';
 
 export const Navbar = () => {
   return (
-		<div className="flex items-center w-full bg-[#def5f1] p-4">
-			<img src={logo} className="App-logo w-16 min-h-[unset]" alt="logo" />
+		<div className="flex items-center w-full h-fit bg-[#def5f1] p-4">
+			<img src={logo} className="App-logo w-16 h-fit min-h-[unset]" alt="logo" />
 			<span>Navbar</span>
 		</div>
   );
